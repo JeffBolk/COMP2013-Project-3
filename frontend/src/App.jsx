4808b205ef5059a5325f7377634543aa88bc5551
Maxwell Schriner <max.schriner@student.sl.on.ca>
2025-12-10 17:00:02 -0500

    added in the routes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import products from "./data/products";
 import GroceriesAppContainer from "./Components/GroceriesAppContainer";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <GroceriesAppContainer products={products} />
+      <Router>
+        <Routes>
+          <Route path="/" element="" />
+          <Route path="/create-user" element="" />
+          <Route path="/main" element={<GroceriesAppContainer products={products} />}/>
+          <Route path="/edit-product" element="" />
+          <Route path="/add-product" element="" />
+          <Route path="/not-authorized" element="" />
+          <Route path="*" element="" />
+        </Routes>
+      </Router>
     </>
   );
 }
